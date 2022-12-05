d8fc745f6c943429e3df423c9f5ed7165abbbf2c
Илья Дудкин <iluha805@gmail.com>
2022-12-05 17:40:45 +0300

    FixGetAsync

---
+++ CSharpLab4/Models/UserViewModels/TeamIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpLab4.Models.UserViewModels
+{
+    public class TeamIndexData
+    {
+        public IEnumerable<Player> Players { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+    }
+}
--- CSharpLab4/Pages/Teams/Index.cshtml.cs
+++ CSharpLab4/Pages/Teams/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using CSharpLab4.Models.UserViewModels;
 using CSharpLab4.Data;
 using CSharpLab4.Models;
 
@@ -20,13 +21,30 @@ namespace CSharpLab4.Pages.Teams
         }
 
         public IList<Team> Team { get;set; } = default!;
+        public int TeamID { get; set; }
+        public int PlayerID { get; set; }
+        public TeamIndexData TeamData { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? playerID, int? teamID)
         {
-            if (_context.Teams != null)
+            TeamData = new TeamIndexData();
+            TeamData.Teams = await _context.Teams
+                .Include(i => i.Players)
+                .OrderBy(i => i.Name)
+                .ToListAsync();
+            if(teamID!=null)
             {
-                Team = await _context.Teams
-                    .Include(c => c.Coach).AsNoTracking().ToListAsync();
+                TeamID = teamID.Value;
+                Team team = TeamData.Teams
+                    .Where(i => i.TeamID == teamID.Value).Single();
+                TeamData.Players = team.Players;
+            }
+            if(playerID!=null)
+            {
+                PlayerID = playerID.Value;
+                Player player = TeamData.Players
+                    .Where(i => i.ID== playerID.Value).Single();
+                TeamData.Teams = player.Teams;
             }
         }
     }
